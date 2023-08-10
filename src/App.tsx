ee6859b316eb7a65673b007af41a62db43be9a57
Marshall Ku <marshall@kakao.com>
2023-08-10 23:34:06 +0900

    Add logics for infinite swipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Calendar } from "./components";
 
 function App() {
-    return <Calendar date={new Date()} />;
+    return (
+        <Calendar
+            date={new Date()}
+            onMonthChange={(date) => {
+                console.log(`Month changed: ${date}`);
+            }}
+        />
+    );
 }
 
 export default App;
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import type SwiperClass from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { add, eachMonthOfInterval, format, parse, sub } from "date-fns";
 import { classNames } from "@utils";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { Month } from "@components";
 import styles from "./index.module.scss";
 
 interface CalendarProps {
     date?: Date;
     onDateClick?(date: Date): void;
+    onMonthChange?(date: Date): void;
 }
 
 const INITIAL_SLIDE = 2;
@@ -15,7 +17,11 @@ const MONTH_FORMAT = "yyyy-MM";
 
 const cx = classNames(styles, "calendar");
 
-function Calendar({ date = new Date(), onDateClick }: CalendarProps) {
+function Calendar({
+    date = new Date(),
+    onDateClick,
+    onMonthChange,
+}: CalendarProps) {
     const [currentDate, setCurrentDate] = useState(date);
     const currentMonth = useMemo(
         () => format(currentDate, MONTH_FORMAT),
@@ -31,6 +37,43 @@ function Calendar({ date = new Date(), onDateClick }: CalendarProps) {
             end: add(firstDayOfCurrentMonth, { months: 2 }),
         })
     );
+    const indexCache = useRef(INITIAL_SLIDE);
+
+    const handleMonthTransitionChange = useCallback(
+        (swiper: SwiperClass, date?: Date) => {
+            if (!swiper) {
+                MediaElementAudioSourceNode;
+            }
+
+            if (indexCache.current === swiper.realIndex && !date) {
+                return;
+            }
+
+            indexCache.current = swiper.realIndex;
+            setMonthSlides((prev) => {
+                const { realIndex: startIndex } = swiper;
+                const currentDate = date || prev[startIndex];
+                const newInterval = eachMonthOfInterval({
+                    start: sub(currentDate, { months: 2 }),
+                    end: add(currentDate, { months: 2 }),
+                });
+                const nextState: typeof prev = [];
+
+                for (let i = 0, max = prev.length; i < max; ++i) {
+                    const currentIndex = (startIndex + i) % max;
+                    const SWIPER_INDEX = [2, 3, 4, 0, 1];
+                    nextState[currentIndex] = newInterval[SWIPER_INDEX[i]];
+                }
+
+                console.log(nextState.map((x) => x.getMonth() + 1));
+
+                onMonthChange?.(nextState[startIndex]);
+
+                return nextState;
+            });
+        },
+        [onMonthChange]
+    );
 
     useEffect(() => {
         setCurrentDate(date);
@@ -41,8 +84,10 @@ function Calendar({ date = new Date(), onDateClick }: CalendarProps) {
             <div className={cx("__container")}>
                 <Swiper
                     slidesPerView={1}
+                    loop
                     centeredSlides
                     initialSlide={INITIAL_SLIDE}
+                    onTransitionEnd={handleMonthTransitionChange}
                 >
                     {0 < monthSlides.length &&
                         monthSlides.map((month) => (
